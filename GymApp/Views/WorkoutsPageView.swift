93e49d1eda6fe84a0854931d05b34bc851c7c62b
Logan Proffitt <lopr6007@colorado.edu>
2024-07-31 15:32:10 -0600

    Switched to compact date picker and moved to the top of the screen for cleaner look. Might eventually want adding functionality on the bottom, but will need to figure out how to best design the UI. It also could be worth moving the top bar into swift's toolbar feature.

--- GymApp/Views/WorkoutsPageView.swift
+++ GymApp/Views/WorkoutsPageView.swift
@@ -5,99 +5,59 @@ struct WorkoutsPageView: View {
     @ObservedObject var workoutsController = WorkoutGroupsController.shared
     @State private var selectedWorkoutID: ObjectId? = nil
     @State private var date = Date()
-    @State var calendarVisible = false
 
     var body: some View {
         NavigationStack {
             
-            ZStack {
-                VStack(spacing: 5) {
-                    
-                    // Workout text
-                    HStack {
-                        Text("Workouts")
-                            .font(.title)
-                            .bold()
-                            .padding(.leading)
-                        Spacer()
-                    }
-                    .padding(.top) // Add padding at the top
-                    
-                    // List view models
-                    List {
-                        WorkoutGroupsView()
-                    }
-                    .scrollDismissesKeyboard(.interactively)
-                }
+            VStack(spacing: 5) {
                 
-                
-                // Calendar
-                if calendarVisible {
+                // Top bar
+                HStack {
+                    // Workouts text
+                    Text("Workouts")
+                        .font(.title)
+                        .bold()
+                        .padding(.leading)
+                    
+                    Spacer()
+                    
+                    // Calendar picker
                     DatePicker(
-                        "Workout Date",
+                        "",
                         selection: $date,
                         displayedComponents: [.date]
                     )
-                    .datePickerStyle(.graphical)
-                    .background(Color.gray)
+                    .datePickerStyle(.compact)
+                    .labelsHidden()
+                    .background(Color.clear)
                     .cornerRadius(10)
                     .shadow(radius: 10)
-                    .padding()
-                    .frame(maxWidth: 400, maxHeight: 400)
-                    .position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height * 2 / 3) // Position the calendar in the center top part of the screen
                     .transition(.slide)
-                }
-
-                
-                // New workout buttons
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        
-                        // Pick date button
-                        Button(action: {
-                            withAnimation {
-                                calendarVisible.toggle()
-                        }}) {
-                                Image(systemName: "calendar.badge.plus")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 48, height: 48)
-                                    .foregroundColor(.yellow)
-                            .padding()
-                        }
-                        
-                        // Add workout button (current date)
-                        Button(action: {
-                            withAnimation {
-                                calendarVisible = false
-                                selectedWorkoutID = workoutsController.addWorkout(providedDate: date)
-                                date = Date()
-                        }}) {
-                                Image(systemName: "plus.circle.fill")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 48, height: 48)
-                                    .foregroundColor(.blue)
-                            .padding()
-                        }
-                        .padding(.trailing)
+                    .padding(.trailing)
+                    // Add workout button (current date)
+                    Button(action: {
+                        withAnimation {
+                            selectedWorkoutID = workoutsController.addWorkout(providedDate: date)
+                            date = Date()
+                    }}) {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.blue)
+                            .padding(.trailing)
                     }
+
                 }
+                .padding(.top) // Add padding at the top
+                
+                // List view models
+                List {
+                    WorkoutGroupsView()
+                }
+                .scrollDismissesKeyboard(.interactively)
+                
             }
-            
-                /*
-            .navigationDestination(isPresented: Binding(
-                get: { selectedWorkoutID != nil },
-                set: { newID in
-                    if !newID { selectedWorkoutID = nil }
-                }
-            )) {
-                let workoutBinding = WorkoutViewModel(day: nil).binding(for: selectedWorkoutID!)
-                WorkoutDetailView(workout: workoutBinding)
-                EmptyView()
-            }*/
         }
     }
 }
